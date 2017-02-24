c3d83e4562177ab93eeca9e865e529c2bc412fd3
leknaat <wishfire@gmail.com>
2017-02-24 16:55:48 +0000

    First part of 12 fixed. On to second part.

--- src/components/question12/text.js
+++ src/components/question12/text.js
@@ -3,6 +3,6 @@ import _ from 'lodash'
 
 export default class Text extends React.Component {
   render() {
-    return (<p>{_.map(this.props)}</p>)
+    return (<p>{this.props.children}</p>)
   }
 }
