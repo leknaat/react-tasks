4a7a888cf27bef16628d21e3e65c9eb131038603
leknaat <wishfire@gmail.com>
2017-02-27 18:45:53 +0000

    Task 14 done, have hit stopping point. Getting work checked

--- src/components/question14/bigbutton.js
+++ src/components/question14/bigbutton.js
@@ -2,12 +2,9 @@ import React from 'react'
 
 export default class BigButton extends React.Component {
 
-  handleClick(e) {
-    e.preventDefault()
-    console.log(this.props.onPress())
-  }
+
 
   render() {
-    return (<button onClick = {this.handleClick.bind(this)}>{this.props.children}</button>)
+    return (<button onClick = {this.props.onPress}>{this.props.children}</button>)
   }
 }
