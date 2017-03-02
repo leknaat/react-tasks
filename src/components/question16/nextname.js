173e70b06cfcbb5fa64a56504b7ea762f8a25a03
leknaat <wishfire@gmail.com>
2017-03-02 12:13:47 +0000

    Working on 16.

--- src/components/question16/nextname.js
+++ src/components/question16/nextname.js
@@ -1,24 +1,17 @@
 import React, { Component} from 'react'
-import _ from 'lodash'
 
 export default class NextName extends Component {
 
   constructor() {
     super()
-    this.state = {names: "Richard"}
-  }
-
-  allNames() {
-    return _.forEach(this.props.names, (name) => {
-      return ({name})
-    })
+    this.state = {name: "Richard"}
   }
 
   render(){
     return (
       <div>
-        <p>Richard</p>
-        <button onClick={() => this.setState({ names: this.allNames() })}>Next</button>
+        <p>{this.state.name}</p>
+        <button onClick={() => this.setState({ name: this.props.names[1] })}>Next</button>
       </div>
     )
   }
