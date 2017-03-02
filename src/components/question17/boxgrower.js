b7603ea2ea8b5a6c9b2a879047500b2474ae521d
leknaat <wishfire@gmail.com>
2017-03-02 17:00:43 +0000

    17 done, on to 18.

--- src/components/question17/boxgrower.js
+++ src/components/question17/boxgrower.js
@@ -4,14 +4,14 @@ export default class BoxGrower extends Component {
 
 constructor(){
   super()
-  const adderBase = {background: '#aa0058', height: 20}
+  const adderBase = {style: {background: '#aa0058', height: 20}}
   this.state = adderBase
 }
 
 boxGrower(){
-  const currentHeight= this.state.height
+  const currentHeight= this.state.style.height
   const addedHeight = currentHeight + 20
-  this.setState({ background: '#aa0058', height: this.addedHeight})
+  this.setState({style: { background: '#aa0058', height: addedHeight}})
   console.log(addedHeight)
 
 }
@@ -19,7 +19,7 @@ boxGrower(){
   render(){
     return (
       <div>
-        <p style={this.state}>Box with coloured background</p>
+        <p style={this.state.style}>Box with coloured background</p>
       <button onClick={()=> this.boxGrower()} >Grow</button>
       </div>
 
